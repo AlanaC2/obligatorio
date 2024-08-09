fde5a7fc9f01387b42db4fbea1fc730a141222cf
AlanaC2 <alanacorbo15@gmail.com>
2024-08-08 23:29:17 -0300

    Update IASS (final).java
    Versión final del cálculo de IASS

--- IASS (final).java
+++ IASS (final).java
@@ -8,37 +8,45 @@ public class IASS {
 
 		        Scanner lector = new Scanner(System.in);
 
-		        double BPC =  6177;
+		      double BPC =  6177;
 
 		        double limite1 = 9 * BPC;
 		        double limite2 = 15 * BPC;
 		        double limite3 = 50 * BPC;
 
+		        System.out.println("Elegiste calcular el IASS");
 		        System.out.println("Ingresa sus ingresos mensuales: ");
-		        int iM = lector.nextInt();
+		        int iM1 = lector.nextInt();
 
 		        double iass = 0;
-		        
 
-		        if (iM > 0 && iM <= limite1) {
+
+		        if (iM1 > 0 && iM1 <= limite1) {
 		            iass = 0;
-		          
+		            System.out.println("Usted no debe de pagar el IASS");
 
-		        } else if (iM > limite1 && iM <= limite2) {
-		            iass = (iM - limite1) * 0.08;
-		           
-
-		        } else if (iM > limite2 && iM <= limite3) {
-		            iass = ((iM - limite2) * 0.24) + ((limite2 - limite1) * 0.08);
+		        } else if (iM1 > limite1 && iM1 <= limite2) {
+		            iass = (iM1 - limite1) * 0.08;
 		            
+		            System.out.println("Su porcentaje de impuesto es 8%");
+		            System.out.println("El impuesto a pagar es: " + iass);
+		            System.out.println("Su sueldo luego del impuesto es de: " + (iM1 - iass));
 
-		        } else if (iM > limite3) {
-		            iass = ((iM - limite3) * 0.30) + ((limite3 - limite2)) * 0.24 + ((limite2 - limite1) * 0.08);
-		          
+		        } else if (iM1 > limite2 && iM1 <= limite3) {
+		            iass = ((iM1 - limite2) * 0.24) + ((limite2 - limite1) * 0.08);
+		            
+		            System.out.println("Su porcentaje de impuesto es 24%");
+		            System.out.println("El impuesto a pagar es: " + iass);
+		            System.out.println("Su sueldo luego del impuesto es de: " + (iM1 - iass));
+
+		        } else if (iM1 > limite3) {
+		            iass = ((iM1 - limite3) * 0.30) + ((limite3 - limite2)) * 0.24 + ((limite2 - limite1) * 0.08);
+		            
+		            System.out.println("Su porcentaje de impuesto es 30%");
+		            System.out.println("El impuesto a pagar es: " + iass);
+		            System.out.println("Su sueldo luego del impuesto es de: " + (iM1 - iass));
 		        }
 
-		      
-		        System.out.println("El impuesto a pagar es: " + iass);
-		    
-		    }
+
 		}
+}
